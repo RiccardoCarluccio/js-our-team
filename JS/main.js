6b5f59a0b4660332f27243f14464bc66d2ddae11
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-14 15:29:33 +0200

    Array of objects created

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+const TEAM_MEMBERS = [
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    jobPosition: 'Founder & CEO',
+    profilePic: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Caroll',
+    jobPosition: 'Chief Editor',
+    profilePic: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    firstName: 'Walter',
+    lastName: 'Gordon',
+    jobPosition: 'Office Manager',
+    profilePic: 'walter-gordon-office-manager.jpg'
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Lopez',
+    jobPosition: 'Social Media Manager',
+    profilePic: 'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    firstName: 'Scott',
+    lastName: 'Estrada',
+    jobPosition: 'Developer',
+    profilePic: 'scott-estrada-developer.jpg'
+  },
+  {
+    firstName: 'Barbara',
+    lastName: 'Ramos',
+    jobPosition: 'Graphic Designer',
+    profilePic: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
