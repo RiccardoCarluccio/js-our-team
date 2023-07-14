e4942e86e2e9c15ca3bc2c7d3cd3beb8f242601c
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-14 17:31:09 +0200

    layout mini fix

--- JS/main.js
+++ JS/main.js
@@ -42,9 +42,16 @@ TEAM_MEMBERS.forEach(object => console.log(object));
 for (i = 0; i < TEAM_MEMBERS.length; i++) {
   const SINGLE_MEMBER = TEAM_MEMBERS[i];
 
-  document.getElementById('team-container').innerHTML += `<div>${SINGLE_MEMBER.firstName} ${SINGLE_MEMBER.lastName} ${SINGLE_MEMBER.jobPosition} ${SINGLE_MEMBER.profilePic}</div>`;
-
-  document.getElementById('team-container').innerHTML += `<img src="IMG/${SINGLE_MEMBER.profilePic}" alt="Profile Picture"> <br>`;
+  document.getElementById('team-container').innerHTML += 
+    `
+      <div class="mini-container">
+      <div>
+        <div>${SINGLE_MEMBER.firstName} ${SINGLE_MEMBER.lastName} ${SINGLE_MEMBER.jobPosition}</div>
+        <img src="IMG/${SINGLE_MEMBER.profilePic}" alt="Profile Picture">
+      </div>
+      
+      </div>
+    `;
 
   //da riprovare a fare con .createElement() e/o .append()
 }
